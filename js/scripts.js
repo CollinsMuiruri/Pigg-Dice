5c5627fb23aae657bb415bc359d407bcba0eff72
Collins Muiruri <wanyekicollins@gmail.com>
2022-04-18 02:30:18 +0300

    initialize values on frontend

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// get access to global elements
+let score0 = document.getElementById('score--0');
+let score1 = document.getElementById('score--1');
+
+let dice = document.querySelector('.dice');
+
+// intitialize the values to 0
+score0.textContent = 0;
+score1.textContent = 0;
+
+dice.classList.add('hidden');
